4cfa30cd469c413a49067dfdcd9f924409dffbca
Tamerlan Musayev <timoxa_musaev@mail.ru>
2022-04-20 07:30:13 +0400

    yandex

--- src/main.js
+++ src/main.js
@@ -3,11 +3,20 @@ import App from "./App.vue";
 import router from "./router.js";
 import store from "./store.js";
 import "./plugins/base";
+import VueYandexMetrika from "vue-yandex-metrika-ts";
 import vuetify from "./plugins/vuetify";
 Vue.use(router);
 Vue.use(store);
 Vue.config.productionTip = false;
 Vue.prototype.$log = console.log.bind(console);
+
+Vue.use(VueYandexMetrika, {
+  id: 88442766,
+  clickmap: true,
+  trackLinks: true,
+  accurateTrackBounce: true,
+  webvisor: true,
+});
 new Vue({
   render: (h) => h(App),
   router,
